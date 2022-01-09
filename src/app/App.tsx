f8fba36e5a15db657ed6c70b752e7296dd2764ed
Daniel Zotti <daniel.zotti87@gmail.com>
2022-01-09 17:26:50 +0100

    Get rid fo BrowserRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 import 'normalize.css'; // Note this
 
-import { Provider } from 'react-redux';
-import { store } from './App.store';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { HomePage } from '../features/home/HomePage';
+// import { Provider } from 'react-redux';
+// import { store } from './App.store';
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { HomePage } from '../features/home/HomePage';
+import { AudioPlayground } from '../components/Audio';
 
 function App() {
   return (
-    <Provider store={ store }>
+    <AudioPlayground/>
+    /*<Provider store={ store }>
       <BrowserRouter>
         <Routes>
           <Route path="" element={ <HomePage/> }/>
         </Routes>
       </BrowserRouter>
-    </Provider>
+    </Provider>*/
   );
 }
 

12184f3ad5b9591403c738c9f3b600e06a1fce5b
Daniel Zotti <daniel.zotti87@gmail.com>
2022-01-09 17:42:55 +0100

    Fix to BrowserRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
-import 'normalize.css'; // Note this
+import 'normalize.css';
 
-// import { Provider } from 'react-redux';
-// import { store } from './App.store';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import { HomePage } from '../features/home/HomePage';
-import { AudioPlayground } from '../components/Audio';
+import { Provider } from 'react-redux';
+import { store } from './App.store';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HomePage } from '../features/home/HomePage';
+import { Header } from '../components/Header';
+import { CounterPage } from '../features/counter/CounterPage';
 
 function App() {
   return (
-    <AudioPlayground/>
-    /*<Provider store={ store }>
-      <BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter basename={ process.env.PUBLIC_URL }>
+        <Header/>
         <Routes>
-          <Route path="" element={ <HomePage/> }/>
+          <Route path="/" element={ <HomePage/> }/>
+          <Route path="counter" element={ <CounterPage/> }/>
         </Routes>
       </BrowserRouter>
-    </Provider>*/
+    </Provider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,5 +3,5 @@ import { Link } from 'react-router-dom';
 
 export const Header: React.VFC = () => <ul>
   <li><Link to="/">Home</Link></li>
-  <li><Link to="counter">Audio</Link></li>
+  <li><Link to="counter">Counter</Link></li>
 </ul>;
